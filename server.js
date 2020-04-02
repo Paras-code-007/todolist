4990f2c745c897a7a16afc52392ba96e49bd1749
Paras-code-007 <56780209+Paras-code-007@users.noreply.github.com>
2020-04-02 15:29:18 +0530

    debbuged the code and now working perfectly

--- server.js
+++ server.js
@@ -9,42 +9,43 @@ let todos=[{
     done: "no"
 }]
 
-//server.use(express.json)
-//server.use(express.urlencoded)
-server.use(function () {
-    console.log("decoded")
-})
+server.use(express.json())
+server.use(express.urlencoded({extended: true}))
+
+// server.use(function () {
+//     console.log("decoded")
+// })
 
 
 server.get('/todos',function (req,res) {
-    console.log("displayed")
+    //console.log("displayed")
     res.send(todos)
 })
 
 server.post('/todos', function (req,res) {
     todos.push({
         task: req.body.task,
-        done: req.body.done 
+        done: req.body.done
     })
     res.send(todos)
-    console.log("added")
+    //console.log("added")
 })
 
 server.get('/todos/:id', function (req,res) {
     res.send(todos[req.params.id])
-    console.log("added")
+    //console.log("displayed")
 })
 
 server.patch('/todos/:id/', function (req,res) {
-    todos[req.params.id].task= req.body.task //no colon here , we will have = here because we are writing a statement 
+    todos[req.params.id].task= req.body.task //no colon here , we will have = here because we are writing a statement
     todos[req.params.id].done= req.body.done
     res.send(todos)
-    console.log("replaced")
+    //console.log("replaced")
 })
 server.delete('/todos/:id',function (req,res) {
     todos.splice(req.params.id,1)
     res.send(todos)
-    console.log("deleted")
+    //console.log("deleted")
 })
 
 server.listen(2121)
--- try.js
+++
@@ -1,5 +0,0 @@
-let arr=[12,34,45,67,89,23,56,87]
-
-arr.splice(3,1)
-
-console.log(arr)
